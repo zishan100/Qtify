dd3790a3a170158fb62d23a824b2849d1ad0a99d
zishan100 <zishanansari111@gmail.com>
2024-02-28 23:04:44 +0530

    Top & New Albums component completed

---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './Card.module.css'
+import { Chip, Typography } from '@mui/material'
+
+export default function Card({cardObj}) {
+  return (
+    <div className={style.container} >
+       <div className={style.imageCard} >
+            <img src={cardObj.image} alt={cardObj.title} width={159} height={170} />
+            <Chip className={style.chipButton} label={`${cardObj.follows} Follows`} variant='outlined' />
+       </div> 
+       <Typography variant='body1' mt={1} gutterBottom >{cardObj.title}</Typography>
+    </div>
+  )
+}
